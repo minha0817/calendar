4a42df15d0694adc58328fe33cacaa9b956c8a35
minha0817 <minhakk24@gmail.com>
2023-03-08 16:19:11 -0800

    Add Day component

---
+++ src/components/Day.jsx
@@ -0,0 +1,15 @@
+import React from "react";  
+
+export default function Day () {
+  return (
+    <ul>
+      <li>Sun</li>
+      <li>Mon</li>
+      <li>Tue</li>
+      <li>Wed</li>
+      <li>Thu</li>
+      <li>Fri</li>
+      <li>Sat</li>
+    </ul>
+  )
+}
