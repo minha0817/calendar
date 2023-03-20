a9166ca7d6e049d42ab45dd7c2dce310c8fc5f13
minha0817 <minhakk24@gmail.com>
2023-03-20 14:10:55 -0700

    Add habit

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from 'react'
 
-export default function Modal () {
+export default function Modal() {
+  const habitList = ['Coding', 'Work out', 'Reading a book']
 
-  const habitList = ["Coding", "Work out", "Reading a book"];
+  const [input, setInput] =  useState('');
+  const [habit, setHabitList] = useState(habitList);
+
+  const addHabitList = (input) => {
+    setHabitList((prev) => {
+      const updatedHabitList = [...prev, input]
+      return updatedHabitList;
+    })
+  }
+
+  const handleClickAdd = (event) => {
+    event.preventDefault();
+    addHabitList(input);
+    setInput('');
+  }
 
   return (
     <div>
       <h2>Habit tracker</h2>
-      {habitList.map((habit) => {
-        return <li key={habit}>
-          {habit}
-        </li>
+      {habit.map((habit) => {
+        return <li key={habit}>{habit}</li>
       })}
+      <form>
+        <input name="Add habits" value={input} placeholder="Add Habit!" onChange={(event) => {setInput(event.target.value)}}/>
+        <button onClick={handleClickAdd}>Add</button>
+      </form>
     </div>
   )
 }
